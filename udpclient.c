ba1deffe6fe6bbc56b0c998f9b738cf4a04c7559
kmb32123 <breyk@WKS003591.ion.uwplatt.edu>
2014-09-30 15:42:27 -0500

    Commonized fucntions of sender and reciever

--- udpclient.c
+++ udpclient.c
@@ -1,33 +1,33 @@
 /*
- * udpclient.c
- *
- * OS: FreeBSD
- * compiler: cc
- *      To compile: cc -o udpclient -g udpclient.c
- *
- * Protocol Independant (working for both IPV4 and IPV6)
- *
- * udpclient assumes udpserver.c is running first.
- * It sends 'count' packets to the server which in turn writes
- * them back to the client; i.e., the server provides an echo service.
- *
- * To run:
- *      1. first run udpserver on some udp port that is not used.
- *      (you can make sure ports are not used with %netstat -a | grep udp)
- *
- *      % udpserver 10000&
- *
- *      2. then run the client
- *
- *      %udpclient localhost 10000 512
- *      %udpclient mchugh.cs.pdx.edu 10000 512
- *      %udpclient 131.252.215.20 10000 512
- *      %udpclient 2001:468:1f04:2f0:250:daff:fe83:d48c 10000 512
- *
- * syntax:
- *      % udpclient remote_host udp_port number_of_packets_to_echo
- *
- */
+* udpclient.c
+*
+* OS: FreeBSD
+* compiler: cc
+*      To compile: cc -o udpclient -g udpclient.c
+*
+* Protocol Independant (working for both IPV4 and IPV6)
+*
+* udpclient assumes udpserver.c is running first.
+* It sends 'count' packets to the server which in turn writes
+* them back to the client; i.e., the server provides an echo service.
+*
+* To run:
+*      1. first run udpserver on some udp port that is not used.
+*      (you can make sure ports are not used with %netstat -a | grep udp)
+*
+*      % udpserver 10000&
+*
+*      2. then run the client
+*
+*      %udpclient localhost 10000 512
+*      %udpclient mchugh.cs.pdx.edu 10000 512
+*      %udpclient 131.252.215.20 10000 512
+*      %udpclient 2001:468:1f04:2f0:250:daff:fe83:d48c 10000 512
+*
+* syntax:
+*      % udpclient remote_host udp_port number_of_packets_to_echo
+*
+*/
 
 #include <sys/types.h>
 #include <sys/socket.h>
@@ -41,43 +41,44 @@ extern int errno;
 
 int createSocket(struct addrinfo * addr)
 {
-    printf("Creating new socket with given address info\n");
-    
-    int sock = socket(addr->ai_family,addr->ai_socktype,addr->ai_protocol);
-    if (sock<0) {
-        printf("Error occurred creating socket so now I am sad");
-        return -1;
-    }
-    
-    printf("Successfully created socket: %ld\n", sock);
-    return sock;
-}
+  printf("Creating new socket with given address info\n");
 
+  int sock = socket(addr->ai_family,addr->ai_socktype,addr->ai_protocol);
+  if (sock<0)
+  {
+    printf("Error occurred creating socket so now I am sad");
+    return -1;
+  }
+
+  printf("Successfully created socket: %ld\n", sock);
+  return sock;
+}
 
 struct addrinfo * createAddressInfo(const char* address, const char* port)
 {
-    printf("Creating address info for %s:%s\n", address, port);
-    
-	int n;
-	struct addrinfo hints, *res;
-    /*initilize addrinfo structure*/ 
-    bzero(&hints, sizeof(struct addrinfo));
-    hints.ai_flags=AI_PASSIVE;
-    hints.ai_family= AF_UNSPEC;
-    hints.ai_socktype= SOCK_DGRAM;
-    hints.ai_protocol=IPPROTO_UDP;
+  printf("Creating address info for %s:%s\n", address, port);
 
-    if((n = getaddrinfo(address, port, &hints, &res)) !=0) {
-        printf("udpclient error for %s: %s",port,gai_strerror(n));
-		return NULL;
-    }
-    printf("Successfully created address info.\n");
+  int n;
+  struct addrinfo hints, *res;
+  /*initilize addrinfo structure*/ 
+  bzero(&hints, sizeof(struct addrinfo));
+  hints.ai_flags=AI_PASSIVE;
+  hints.ai_family= AF_UNSPEC;
+  hints.ai_socktype= SOCK_DGRAM;
+  hints.ai_protocol=IPPROTO_UDP;
 
-    return res;
+  if((n = getaddrinfo(address, port, &hints, &res)) !=0)
+  {
+    printf("Error creating address for %s: %s",port,gai_strerror(n));
+    return NULL;
+  }
+  printf("Successfully created address info.\n");
+
+  return res;
 }
 
 int main (int argc, char **argv){
-  
+
   int sockfd, rc;
   socklen_t salen;
   struct sockaddr *sa;
@@ -85,22 +86,23 @@ int main (int argc, char **argv){
   char *host, *port, *count;
   int i;
   int fromlen;
- 
+
   if(argc != 4)
-   { printf("usage: tcpclient <hostname/IPaddress> <portnumber> <count> \n");
-     return 0;
-   }
+  {
+    printf("usage: tcpclient <hostname/IPaddress> <portnumber> <count> \n");
+    return 0;
+  }
 
   host=argv[1];
   port=argv[2];
   count=atoi(argv[3]);
- 
+
   res = createAddressInfo(host, port);
-  
+
   ressave=res;
 
   sockfd=createSocket(res);
-	
+
   sa=malloc(res->ai_addrlen);
   memcpy(sa, res->ai_addr, res->ai_addrlen);
   salen=res->ai_addrlen;
@@ -109,30 +111,17 @@ int main (int argc, char **argv){
 
   for ( i = 0; i < count; i++) {
 
-   printf("I am TX and I am going to send a %d\n", i);
+    printf("I am TX and I am going to send a %d\n", i);
 
-   /* write msg to remote system
-   * sock
-   * buf
-   * sizeof (union msg)
-   * 0,
-   * client
-   * sizeof (client)
-   */
-   if( rc = sendto(sockfd,&i,sizeof (i),0,sa, salen) <0 ) {
-	/* buffers aren't available locally at the moment,
-	 * try again.
-	 */
-	if (errno == ENOBUFS)
-		continue;
-	perror("sending datagram");
-	return -1;
-   }
+    if( rc = sendto(sockfd,&i,sizeof(i),0,sa, salen) < 0 )
+    {
+      if (errno == ENOBUFS)
+        continue;
+      perror("sending datagram");
+      return -1;
+    }
 
- }/*end of for */ 
+  }/*end of for */ 
   close(sockfd);
   return 0;
-
 }
-
-static char bufval =  'a';
--- udpserver.c
+++ udpserver.c
@@ -1,23 +1,23 @@
 /*
- * udpserver.c
- *
- * This is an example TCP/IP UDP socket server.
- * It will read packets sent to 'portno' and write them back
- * to the sender.  It has no reliability functions.
- * It will loop forever and must be killed in order to make
- * it terminate.
- *
- * Protocol independant( working for both IPV4 and IPV6)
- * syntax:
- *      % udpserver portno &
- *
- * Start the server first and then start the udpclient.c app to
- * send packets to it.  Use the debug switch with udpclient.c to
- * see if any of the packets disappear.  They won't disappear on
- * localhost, but they might go away if they are crossing a busy 
- * router.
- */
- 
+* udpserver.c
+*
+* This is an example TCP/IP UDP socket server.
+* It will read packets sent to 'portno' and write them back
+* to the sender.  It has no reliability functions.
+* It will loop forever and must be killed in order to make
+* it terminate.
+*
+* Protocol independant( working for both IPV4 and IPV6)
+* syntax:
+*      % udpserver portno &
+*
+* Start the server first and then start the udpclient.c app to
+* send packets to it.  Use the debug switch with udpclient.c to
+* see if any of the packets disappear.  They won't disappear on
+* localhost, but they might go away if they are crossing a busy 
+* router.
+*/
+
 #include <stdio.h>
 #include <sys/types.h>
 #include <sys/socket.h>
@@ -28,42 +28,42 @@
 #include <errno.h>
 #include <signal.h>
 
-#define MAXBUF	10 * 1024
-
 int createSocket(struct addrinfo * addr)
 {
-    printf("Creating new socket with given address info\n");
-    
-    int sock = socket(addr->ai_family,addr->ai_socktype,addr->ai_protocol);
-    if (sock<0) {
-        printf("Error occurred creating socket so now I am sad");
-        return -1;
-    }
-    
-    printf("Successfully created socket: %ld\n", sock);
-    return sock;
+  printf("Creating new socket with given address info\n");
+
+  int sock = socket(addr->ai_family,addr->ai_socktype,addr->ai_protocol);
+  if (sock<0)
+  {
+    printf("Error occurred creating socket so now I am sad");
+    return -1;
+  }
+
+  printf("Successfully created socket: %ld\n", sock);
+  return sock;
 }
 
 struct addrinfo * createAddressInfo(const char* address, const char* port)
 {
-    printf("Creating address info for %s:%s\n", address, port);
-    
-	int n;
-	struct addrinfo hints, *res;
-    /*initilize addrinfo structure*/ 
-    bzero(&hints, sizeof(struct addrinfo));
-    hints.ai_flags=AI_PASSIVE;
-    hints.ai_family= AF_UNSPEC;
-    hints.ai_socktype= SOCK_DGRAM;
-    hints.ai_protocol=IPPROTO_UDP;
+  printf("Creating address info for %s:%s\n", address, port);
 
-    if((n = getaddrinfo(address, port, &hints, &res)) !=0) {
-        printf("udpserver error for %s: %s",port,gai_strerror(n));
-		return NULL;
-    }
-    printf("Successfully created address info.\n");
+  int n;
+  struct addrinfo hints, *res;
+  /*initilize addrinfo structure*/ 
+  bzero(&hints, sizeof(struct addrinfo));
+  hints.ai_flags=AI_PASSIVE;
+  hints.ai_family= AF_UNSPEC;
+  hints.ai_socktype= SOCK_DGRAM;
+  hints.ai_protocol=IPPROTO_UDP;
 
-    return res;
+  if((n = getaddrinfo(address, port, &hints, &res)) !=0)
+  {
+    printf("Error creating address for %s: %s",port,gai_strerror(n));
+    return NULL;
+  }
+  printf("Successfully created address info.\n");
+
+  return res;
 }
 
 int main(int argc, char **argv)
@@ -76,47 +76,46 @@ int main(int argc, char **argv)
   struct addrinfo *res, *ressave;
   int current = 0;
 
- if(argc== 2){
-   port=argv[1];
- }
- else
- {  printf("usage: udpserver <portnumber>\n");
+  if(argc== 2)
+  {
+    port=argv[1];
+  }
+  else
+  {
+    printf("usage: udpserver <portnumber>\n");
     return 0;
- }  
+  }  
 
- 
- res = createAddressInfo(NULL, port);
+  res = createAddressInfo(NULL, port);
 
- ressave=res;
+  ressave=res;
 
- sockfd=createSocket(res);
+  sockfd=createSocket(res);
 
- bind(sockfd, res->ai_addr, res->ai_addrlen);
-       
- if(&addrlen)
-   addrlen=res->ai_addrlen;
- 
- freeaddrinfo(ressave);
+  bind(sockfd, res->ai_addr, res->ai_addrlen);
 
- cliaddr=malloc(addrlen);
+  if(&addrlen)
+    addrlen=res->ai_addrlen;
 
- len=addrlen;
+  freeaddrinfo(ressave);
 
- while ( 1 ) {     /* do forever */
-	int rc;
-        int foo = 1;
-        int *foo2 = &foo;
-	if ((rc=recvfrom(sockfd, foo2, MAXBUF, 0, cliaddr, &len)) < 0 ) {
-		printf("server error: errno %d\n",errno);
-		perror("reading datagram");
-		return -1;
-	}
-        printf("I am RX and I got a %d\n", *foo2);
-}
+  cliaddr=malloc(addrlen);
 
-/* can't get here, but just in case: close sockets
-*/
-close(sockfd);
-return 0;
+  len=addrlen;
 
+  while ( 1 ) /* do forever */
+  {     
+    int rc;
+    int foo = 1;
+    int *foo2 = &foo;
+    if ((rc=recvfrom(sockfd, foo2, sizeof(foo), 0, cliaddr, &len)) < 0 ) {
+      printf("server error: errno %d\n",errno);
+      perror("reading datagram");
+      return -1;
+    }
+    printf("I am RX and I got a %d\n", *foo2);
+  }
+
+  close(sockfd);
+  return 0;
 } 
